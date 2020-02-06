589057a6fe770a2d154b6653681160da77f461ed
Mark Mngoma <trq0348@blake.co.za>
2020-02-06 16:27:32 +0200

    API Client

---
+++ src/index.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once 'models/APIClient.php';
+$product = new \FiftyOnRed\Blade\models\Product();
---
+++ src/models/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace FiftyOnRed\Blade\models;
+
+use FiftyOnRed\Blade\models\APIClient;
+
+class Product
+{
+    var $BASE_URL;
+    var $apiClient;
+
+
+}
